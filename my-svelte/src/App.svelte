9de63c435eed4a7b8172c021b3f8b5a21364abc4
sanhee <68936@naver.com>
2022-03-03 18:24:17 +0900

    feat: 보간법(내용/속성/표현식)

--- my-svelte/src/App.svelte
+++ my-svelte/src/App.svelte
@@ -1,15 +1,13 @@
 <script>
-   import { tick } from 'svelte'
-
-   let name = 'world' 
-   
-   // tick()은 promise를 반환하는 비동기 함수
-   async function handler() {
-       name = 'Heropy' // 반응성이 있어서, 변화가 생기면 바로 화면이 갱신될 것 같지만, 스벨트는 로직 다 끝나고 화면 갱신됨.
-       await tick(); // 데이터가 갱신되고, 화면이 갱신될때까지 반응성을 기다려줄 수 있음. await가 기다리는 것 
-       const h1 = document.querySelector('h1')
-       console.log(h1.innerText)
-   }
+    let href = 'https://heropy.blog'
+    let name = 'Heropy'
+    let value = 'New input value!'
+    let isUpperCase = false
 </script>
 
-<h1 on:click={handler}>Hello {name}!</h1>
+<a {href}>{name}</a>
+
+<input {value} on:input={e => value = e.target.value}/>
+<input bind:value />
+
+<div>{isUpperCase ? 'DIV' : 'div'}</div>
