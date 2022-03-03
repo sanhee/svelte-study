a156f4e175d897a0844c292c7dd180259f0a2ce9
sanhee <68936@naver.com>
2022-03-03 13:46:57 +0900

    feat: 커밋 작성자 수정

---
+++ my-svelte/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Something from './Something.svelte'
+
+    let toggle = false
+</script>
+
+<button on:click={() => {toggle = !toggle}}>
+  Toggle
+</button>
+
+{#if toggle}
+    <Something />
+{/if}
---
+++ my-svelte/src/Something.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { onMount, onDestroy } from 'svelte'
+
+    onMount(() => {
+        console.log('Mounted! 컴포넌트가 화면에 연결되면~!')
+        // const h1 = document.querySelector('h1')
+        // console.log(h1.innerText)
+        return () => {
+            console.log('Destroy in mount')
+        }
+    })
+
+    onDestroy(() =>{
+        console.log('Before destroy~ 컴포넌트가 화면에서 사라지기 직전에 처리할 로직 작성')
+        // const h1 = document.querySelector('h1')
+        // console.log(h1.innerText)
+    })
+</script>
+
+<h1>Something</h1>
