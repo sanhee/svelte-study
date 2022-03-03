18ba2d0b723353c4c663330898b381de34d8a4eb
sanhee <68936@naver.com>
2022-03-04 01:46:00 +0900

    feat: 반응성 구문 패턴 정리

--- my-svelte/src/App.svelte
+++ my-svelte/src/App.svelte
@@ -1,19 +1,62 @@
 <script>
     let count = 0
-    // let double = 0
-
-    // $: {
-    //     double = count * 2
-    // }
 
     $: double = count * 2
 
-    function assign(){
-        count += 1
+    $: {
+        console.log(count)
         console.log(double)
     }
+
+    // 함수 실행
+    $: count, log()
+
+
+    // 즉시 실행 함수 (IIFE)
+    $: count, (() =>{
+        console.log('life: Heropy')
+    })();
+
+    // 조건문
+    $: if(count > 0){
+        console.log('if:', double)
+    }
+
+    //반복문
+    $: for (let i = 0; i < 3 ; i += 1){
+        count
+        console.log('for:', i)
+    }
+
+    // 조건문
+    $: switch (count){
+        case 1:
+            console.log('switch: 1')
+        break
+        default:
+            console.log('switch: default')
+    }
+
+    // 유효범위
+    $: {
+        function scope1(){
+            console.log('scope1')
+            function scope2(){
+                console.log('scope2')
+                function scope3(){
+                    console.log('scope3', count)
+                }
+                scope3() 
+            }
+            scope2()
+        }
+        scope1()
+    }
+
+    function log(){
+        console.log('fn: Heropy!')
+    }
+
 </script>
 
-<button on:click={assign}>Assign!</button>
-<h2>{count}</h2>
-<h2>{double}</h2>
+<button on:click={() => count += 1}> Assign </button>
