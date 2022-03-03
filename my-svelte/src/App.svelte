27cfb3fcd9722356602aa8be77dd42973a7e10e5
sanhee <68936@naver.com>
2022-03-03 17:43:15 +0900

    feat: tick()

--- my-svelte/src/App.svelte
+++ my-svelte/src/App.svelte
@@ -1,13 +1,15 @@
 <script>
-    import { lifecycle, delayRender } from './lifecycle.js'
-    import Something from './Something.svelte'
+   import { tick } from 'svelte'
 
-    let done = delayRender()
-    lifecycle()
+   let name = 'world' 
+   
+   // tick()은 promise를 반환하는 비동기 함수
+   async function handler() {
+       name = 'Heropy' // 반응성이 있어서, 변화가 생기면 바로 화면이 갱신될 것 같지만, 스벨트는 로직 다 끝나고 화면 갱신됨.
+       await tick(); // 데이터가 갱신되고, 화면이 갱신될때까지 반응성을 기다려줄 수 있음. await가 기다리는 것 
+       const h1 = document.querySelector('h1')
+       console.log(h1.innerText)
+   }
 </script>
 
-{#if $done}
-<h1>Hello LifeCycle!</h1>
-{/if}
-
-<Something />
+<h1 on:click={handler}>Hello {name}!</h1>
