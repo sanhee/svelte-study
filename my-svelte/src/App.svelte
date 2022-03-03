fe4d39b615fb432d82a062db2cdbc13e11a49cde
sanhee <68936@naver.com>
2022-03-03 19:07:52 +0900

    feat: 할당, 반응성

--- my-svelte/src/App.svelte
+++ my-svelte/src/App.svelte
@@ -1,13 +1,33 @@
 <script>
-    let href = 'https://heropy.blog'
-    let name = 'Heropy'
-    let value = 'New input value!'
-    let isUpperCase = false
+    let name  = 'Heropy'
+    let fruits = ['Apple', 'Banana', 'Cherry']
+    let user = {
+        name: 'Heropy',
+        depth: {
+            a: 'b'
+        },
+        numbers: [1, 2]
+    }
+    let numbers = user.numbers
+
+    // 할당 연산자를 사용해야지만 반응성을 얻을 수 있다.
+    function assign(){
+        name = 'Neo'
+        fruits = [...fruits, 'Orange']
+        user.name = 'Neo'
+        user.depth.a = 'c'
+        // await tick()
+        // 메서드가 끝나면 갱신되는데, 그때 푸쉬된 것도 반영돼서 그럼.
+        // 위 user.name, user.depth.a 가 없다면 반응성을 못얻어서 반영안됨.
+        user.numbers.push(3)
+        numbers = numbers
+    }
 </script>
 
-<a {href}>{name}</a>
-
-<input {value} on:input={e => value = e.target.value}/>
-<input bind:value />
-
-<div>{isUpperCase ? 'DIV' : 'div'}</div>
+<button on:click ={assign}>Assign!</button>
+<h2>name: {name}</h2>
+<h2>fruits: {fruits}</h2>
+<h2>user name: {user.name}</h2>
+<h2>user depth a: {user.depth.a}</h2>
+<h2>user numbers: {user.numbers}</h2>
+<h2>numbers: {numbers}</h2>
