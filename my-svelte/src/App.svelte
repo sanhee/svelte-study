bd4bcd5dddb99ed10fb166997322b98aea3c3f50
sanhee <68936@naver.com>
2022-03-04 14:34:26 +0900

    feat: 클래스 바인딩 패턴 정리

--- my-svelte/src/App.svelte
+++ my-svelte/src/App.svelte
@@ -1,47 +1,35 @@
 <script>
-let active = false
-let color = 'tomato'
-let white = 'white'
+    let active = true
+    let valid = false
+    let camelCase = true
 
-let color2 = {
-    t: 'tomato',
-    w: '#FFF'
-}
-let letterSpacing = 'letter-spacing: 5px;'
+    function multi() {
+        return 'active multiple-class'
+    }
 </script>
 
-<button on:click={() => {active = !active}}>
-    Toggle!
-</button>
-
-<!-- <div class={active ? 'hello' : ''}> -->
-<!-- class:는 속성이 아님. 스벨트의 지시어임 -->
-<!-- div 태그에 hello라는 클래스를 붙일 건데 그 기준이 active -->
-<div class:hello={active}>
-    Hello
+<div class={active ? 'active' : ''}>
+    3항 연산자 보간
 </div>
 
-<h2 style ="background-color: {color2.t}; color: {color2.w}; {letterSpacing}">
-    Heropy!
-</h2>
+<div class:active={active}>
+    Class Directive(지시어) 바인딩
+</div>
 
+<!-- 이름과 속성이 같다면 바인딩 단축 가능 -->
+<div class:active>
+    Class 지시어 바인딩 단축 형태
+</div>
 
-<style>
-div {
-    width: 120px;
-    height: 200px;
-    background: royalblue;
-    border-radius: 10px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: white;
-    font-size: 20px;
-    transition: .4s;
-}
+<!-- valid는 false라 class이름 생성되지 않음 -->
+<div class:active
+     class:valid
+     class:camelCase
+     class:camel-case = {camelCase}>
+    다중 Class 지시어 바인딩    
+</div>
 
-.hello {
-    width: 250px;
-    background: tomato;
-}
-</style>
+<div class={multi()}>
+    함수 실행
+</div>
+
