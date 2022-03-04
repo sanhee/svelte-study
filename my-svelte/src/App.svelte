20a0684ea2e441515a76d9b8431344c21a898bdb
sanhee <68936@naver.com>
2022-03-04 15:53:34 +0900

    feat: @keyframes 전역화

--- my-svelte/src/App.svelte
+++ my-svelte/src/App.svelte
@@ -1,21 +1,31 @@
-<script>
-    import Fruits from './Fruits.svelte'
-</script>
-
-<h2>App.svelte</h2>
-<ul class="fruits">
-    <li>Apple</li>
-    <li>Banana</li>
-    <li>Cherry</li>
-</ul>
- 
-<Fruits />
+<div class="box"></div>
 
 <style>
-    .fruits{
-        color: red;
+:global(body) {
+    padding: 60px;
+}
+.box {
+    width: 100px;
+    height: 100px;
+    background-color: tomato;
+    border-radius: 10px;
+    animation: zoom .4s infinite alternate
+}
+
+/* @ At-rules : css 골뱅이 규칙 
+   keyframes 규칙
+   zoom이라는 이름의 keyframes 내용 생성
+   keyframes 내용은 css 애니메이션 속성에 사용
+
+   keyframes 내부에 사용할 수 있는 전역화 수식어 : -global-
+*/
+@keyframes -global-zoom {
+    0% {
+        transform: scale(1);
     }
-    :global(.heropy){
-        color: orange;
+
+    100% {
+        transform: scale(1.5);
     }
+}
 </style>
