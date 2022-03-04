947dc2156922f69d2a83c61b08ad7b6574ae7e7d
sanhee <68936@naver.com>
2022-03-04 16:15:40 +0900

    feat: 일반 요소 바인딩(this)

--- my-svelte/src/App.svelte
+++ my-svelte/src/App.svelte
@@ -1,31 +1,23 @@
-<div class="box"></div>
+<script>
+import { tick } from "svelte";
 
-<style>
-:global(body) {
-    padding: 60px;
-}
-.box {
-    width: 100px;
-    height: 100px;
-    background-color: tomato;
-    border-radius: 10px;
-    animation: zoom .4s infinite alternate
-}
 
-/* @ At-rules : css 골뱅이 규칙 
-   keyframes 규칙
-   zoom이라는 이름의 keyframes 내용 생성
-   keyframes 내용은 css 애니메이션 속성에 사용
+    let isShow = false
+    let inputEl
 
-   keyframes 내부에 사용할 수 있는 전역화 수식어 : -global-
-*/
-@keyframes -global-zoom {
-    0% {
-        transform: scale(1);
+    async function toggle() {
+        isShow = !isShow
+        await tick()
+        // const inputEl = document.querySelector('input')
+        inputEl && inputEl.focus()
     }
+</script>
 
-    100% {
-        transform: scale(1.5);
-    }
-}
-</style>
+<!-- 
+    this: input element를 말함 
+    inputEl: input요소와 바인딩할 데이터의 이름
+-->
+<button on:click = {toggle}>Edit!</button>
+{#if isShow}
+    <input bind:this={inputEl} />
+{/if}
