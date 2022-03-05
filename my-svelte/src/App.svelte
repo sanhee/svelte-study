a77682dc289b09256f81a3bb0c7e7e45b929d91e
sanhee <68936@naver.com>
2022-03-05 19:56:28 +0900

    feat: Await 블록

--- my-svelte/src/App.svelte
+++ my-svelte/src/App.svelte
@@ -1,43 +1,33 @@
 <script>
-import axios from 'axios'
+let isError = false
+//let promise = new Promise(resolve => resolve('Hmm..'))
+let promise = Promise.resolve('Hmm...')
 
-let apikey = '9d38c929'
-let title = ''
-let movies = null
-let error = null
-let loading = false
-
-function search() {
-    loading = true;
-    error = null;
-    movies = null;
-
-    const res = axios.get(`http://www.omdbapi.com/?apikey=${apikey}&s=${title}`)
-                   .then((response) => {
-                       movies = response.data.Search
-                       loading = false;
-                    })
-                   .catch((err) => {error = err})
-    
+function fetchName() {
+    return new Promise((resolve, reject) => {
+        if ((isError)) {
+            reject(new Error('Sorry....'))
+        }
+        setTimeout(() => {
+            resolve('Noeul')
+        }, 2000);
+    })
 }
 </script>
 
-<h2>{title}</h2>
-<input bind:value={title}/>
-<button on:click={search}>검색</button>
+<button on:click={() => {
+    promise = fetchName()
+}}>
+    Fetch name!
+</button>
 
-{#if loading}
-    <p style="color:red">loading....</p>
-{:else if movies}
-    <ul>
-        {#each movies as movie}
-        <li>
-            {movie.Title}
-        </li>
-        {/each}
-    </ul>
-{:else if error}
-    <p style="color:green">{error.message}</p>
-{:else}
-    <p>검색결과 없음.</p>
-{/if}
+{#await promise}
+    <!-- 대기(pending...) -->
+    <p>Loading...</p>
+{:then name}
+    <!-- 이행(fullfiled) -->
+    <h1>{name}</h1>
+{:catch error}
+    <!-- 거부(rejected) -->
+    <h1>{error.message}</h1>
+{/await}
